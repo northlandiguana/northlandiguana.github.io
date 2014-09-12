e8653caf14dce2c2b0dbd429d54da9709f2992c6
Carl Sack <northlandiguana@gmail.com>
2014-09-12 17:19:37 -0500

    Boilerplate + hello world

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/***********************************************
+* A JavaScript document by Carl Sack           *
+* Demonstration of JavaScript Principles       *
+* Shamelessly stolen from Rich Donohue         *
+* Creative Commons 3.0 license, 2014           *
+***********************************************/
