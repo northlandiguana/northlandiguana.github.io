49decddfe92de58389daab5ee6ff6363fe0fd6f9
Carl Sack <northlandiguana@gmail.com>
2014-10-20 09:01:15 -0500

    Created D3 demo

---
+++ D3_demo/js/main.js
@@ -0,0 +1,32 @@
+/***********************************************
+* A JavaScript document by Carl Sack           *
+* Demonstration of D3 Principles               *
+* Creative Commons 3.0 license, 2014           *
+***********************************************/
+
+function initialize(){
+
+	var myNumberData = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+	var myStringData = ["this", "that", "theotherthing", "foo", "bar", "bat"];
+	var myColors = ["#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77",	"#980043"];
+
+	createSVG();
+
+	function createSVG(){
+
+		var w = 900, h = 500;
+
+		//always name blocks with a variable, even if it won't be used
+		var container = d3.select("body") //gets the <body> element from the DOM
+			.append("svg") //put a new svg in the body
+			.attr("width", w) //assign the width
+			.attr("height", h) //assign the height
+			.attr("class", "container") //always assign a class (as the block name) for styling and future selection
+			.style("background-color", "rgba(0,0,0,0.2)")
+	};
+
+	function 
+
+};
+
+window.onload = initialize();
